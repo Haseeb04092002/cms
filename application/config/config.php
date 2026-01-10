ef8fc5343dec3243b4e6e7db10a2fe91d25b8a5f
Haseeb04092002 <163792769+Haseeb04092002@users.noreply.github.com>
2026-01-10 17:01:16 +0500

    up

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'https://cms.itimium.com.pk/';
 // $config['base_url'] = 'https://itimium.com/others/pakhtoon-pos-spa';
 
 /*
