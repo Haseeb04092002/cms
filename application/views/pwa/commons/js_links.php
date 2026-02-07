75b10af4b329e9905e215344b5c78a2c74ea5fb4
Haseeb04092002 <163792769+Haseeb04092002@users.noreply.github.com>
2026-02-07 22:20:30 +0500

    up

--- application/views/pwa/commons/js_links.php
+++ application/views/pwa/commons/js_links.php
@@ -66,3 +66,13 @@
 
 <!-- Main Script (Always LAST) -->
 <!-- <script src="<?= base_url('assets/js/script.js'); ?>"></script> -->
+
+<script>
+    if ("serviceWorker" in navigator) {
+        window.addEventListener("load", () => {
+            navigator.serviceWorker.register("/service-worker.js")
+                .then(reg => console.log("SW registered", reg.scope))
+                .catch(err => console.error("SW failed", err));
+        });
+    }
+</script>
